e70e983f099cb314e49d8c26f13dd04d653092eb
Firman Wahyudi <e41190305@student.polije.ac.id>
2023-08-09 15:14:05 +0700

    refactor: added new notification and input in profile

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -7,6 +7,15 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+            @php
+                $message = Session::get('error') ? Session::get('error') : Session::get('success');
+            @endphp
+            @if ($message)
+                <x-notification-message status="{{ $message['status'] }}">
+                    <x-slot name="title">{{ $message['status'] }}</x-slot>
+                    <x-slot name="body">{{ $message['message'] }}</x-slot>
+                </x-notification-message>
+            @endif
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                 <div class="max-w-xl">
                     @include('profile.partials.update-profile-information-form')
@@ -19,11 +28,11 @@
                 </div>
             </div>
 
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+            {{-- <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                 <div class="max-w-xl">
                     @include('profile.partials.delete-user-form')
                 </div>
-            </div>
+            </div> --}}
         </div>
     </div>
 </x-app-layout>
