76672c85bc9a0b95164fdfc5a629c560f3a98e86
Firman Wahyudi <firmanwhyd007@gmail.com>
2023-08-09 23:53:23 +0700

    feat: add filter period in booking index

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -2,16 +2,50 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\BookingCreateRequest;
 use App\Models\Booking;
 use Illuminate\Http\Request;
+use App\Http\Requests\BookingCreateRequest;
 
 class BookingController extends Controller
 {
     public function index(Request $request)
     {
         if ($request->ajax()) {
-            $bookings = Booking::with(['user.supervisor', 'vehicle', 'driver', 'approvedBy'])->get();
+            $query = Booking::with(['user.supervisor', 'vehicle', 'driver', 'approvedBy']);
+            if ($request->user()->role == 'supervisor') {
+                $query->whereHas('user', function ($query) {
+                    $query->where('supervisor_id', auth()->user()->id);
+                });
+            } elseif ($request->user()->role == 'employee') {
+                $query->where('user_id', auth()->user()->id);
+            }
+            if ($request->filled('period')) {
+                $period = $request->input('period');
+                switch ($period) {
+                    case 'today':
+                        $query->whereDate('created_at', now());
+                        break;
+                    case 'one_week':
+                        $query->whereDate('created_at', '>=', now()->subWeek());
+                        break;
+                    case 'one_month':
+                        $query->whereDate('created_at', '>=', now()->subMonth());
+                        break;
+                    case 'three_month':
+                        $query->whereDate('created_at', '>=', now()->subMonths(3));
+                        break;
+                    case 'six_month':
+                        $query->whereDate('created_at', '>=', now()->subMonths(6));
+                        break;
+                    case 'one_year':
+                        $query->whereDate('created_at', '>=', now()->subYear());
+                        break;
+                    default:
+                        // Tidak melakukan filter tambahan jika memilih "All" atau pilihan lainnya
+                        break;
+                }
+            }
+            $bookings = $query->get();
             return datatables()->of($bookings)
                 ->addIndexColumn()
                 ->make(true);
--- resources/views/booking/index.blade.php
+++ resources/views/booking/index.blade.php
@@ -10,21 +10,61 @@
             @if ($message = Session::get('alert'))
                 <x-notification-message :alert="$message" />
             @endif
+            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg mb-3">
+
+                <div class="w-full px-4 ">
+                    <h2 class="text-lg font-medium text-gray-900">
+                        {{ __('Filter Period') }}
+                    </h2>
+                    <div class="flex flex-wrap -mx-3 mb-6">
+                        <div class="w-[1040px] px-3">
+                            <select name="period" id="period"
+                                class="mt-1 block w-full border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm">
+                                <option value="all">
+                                    {{ __('All') }}
+                                </option>
+                                <option value="today">
+                                    {{ __('Today') }}
+                                </option>
+                                <option value="one_week">
+                                    {{ __('One Week') }}
+                                </option>
+                                <option value="one_month">
+                                    {{ __('One Month') }}
+                                </option>
+                                <option value="three_month">
+                                    {{ __('Three Month') }}
+                                </option>
+                                <option value="six_month">
+                                    {{ __('Six Month') }}
+                                </option>
+                                <option value="one_year">
+                                    {{ __('One Year') }}
+                                </option>
+                            </select>
+                        </div>
+                        <div class="max-w-sm px-3 text-right mt-1">
+                            <button type="submit" id="filterButton"
+                                class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                                {{ __('Filter') }}
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                 <section>
                     <header>
-                        <div class="container">
-                            <div class="flex flex-wrap">
-                                <div class="w-full px-4 lg:w-1/2">
-                                    <h2 class="text-lg font-medium text-gray-900">
-                                        {{ __('Booking') }}
-                                    </h2>
-                                </div>
-                                <div class="w-full px-4 lg:w-1/2 text-right">
-                                    <x-link-button-primary href="{{ route('booking.create') }}">
-                                        {{ __('Create') }}
-                                    </x-link-button-primary>
-                                </div>
+                        <div class="flex flex-wrap">
+                            <div class="w-full px-4 lg:w-1/2">
+                                <h2 class="text-lg font-medium text-gray-900">
+                                    {{ __('Booking') }}
+                                </h2>
+                            </div>
+                            <div class="w-full px-4 lg:w-1/2 text-right">
+                                <x-link-button-primary href="{{ route('booking.create') }}">
+                                    {{ __('Create') }}
+                                </x-link-button-primary>
                             </div>
                         </div>
 
@@ -39,6 +79,14 @@
     </div>
     <x-slot name="additionalScripts">
         <script>
+            const buttonFilter = document.querySelector('#filterButton');
+            buttonFilter.addEventListener('click', function() {
+                const selectedPeriod = document.querySelector('#period').value;
+                let url = "{{ route('booking.index') }}" + "?period=" + selectedPeriod;
+                $('#bookingTable').DataTable().ajax.url(url).load();
+            });
+
+
             $(document).ready(function() {
                 $('#bookingTable').DataTable({
                     processing: true,
@@ -98,10 +146,7 @@
                                         statusText = "Rejected by Supervisor";
                                         break;
                                     case 4:
-                                        statusText = "Approved by Employee";
-                                        break;
-                                    case 5:
-                                        statusText = "Approved by Supervisor";
+                                        statusText = "Approved";
                                         break;
                                     default:
                                         statusText = "Unknown";
